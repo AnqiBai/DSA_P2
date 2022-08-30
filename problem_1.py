4e8f32ecea6eb65edfe6c898d1b134ff71862cea
Anqi Bai <baianqi086@gmail.com>
2022-08-30 16:06:00 -0400

    Update solutions for 1 and 3.

---
+++ problem_1.py
@@ -0,0 +1,38 @@
+def sqrt(number):
+    """
+    Calculate the floored square root of a number
+
+    Args:
+       number(int): Number to find the floored squared root
+    Returns:
+       int: Floored Square Root
+    """
+
+   # binary search in the range [0, number]
+    left = 0
+    right = number
+    while left < (right - 1):
+        mid = left + (right - left) // 2
+        if mid * mid == number:
+            return mid
+        elif mid * mid > number:
+            right = mid - 1  # discard the mid value here is how the "floored" is done
+            # If use right = mid here, we will get rounded square root, e.g. 6 for 35
+        else:
+            left = mid
+    diffleft = number - left * left
+    diffright = right ** 2 - number
+    result = left if diffleft < diffright else right
+    return result
+
+
+print("Pass" if (3 == sqrt(9)) else "Fail")
+print("Pass" if (0 == sqrt(0)) else "Fail")
+print("Pass" if (4 == sqrt(16)) else "Fail")
+print("Pass" if (1 == sqrt(1)) else "Fail")
+print("Pass" if (5 == sqrt(27)) else "Fail")
+print("Pass" if (5 == sqrt(25)) else "Fail")
+print("Pass" if (5 == sqrt(26)) else "Fail")
+print("Pass" if (5 == sqrt(35)) else "Fail")
+print("Pass" if (6 == sqrt(36)) else "Fail")
+print("Pass" if (6 == sqrt(37)) else "Fail")
---
+++ problem_3.py
@@ -0,0 +1,97 @@
+import random
+
+
+def swap(array, idx1, idx2):
+    tmp = array[idx1]
+    array[idx1] = array[idx2]
+    array[idx2] = tmp
+
+
+def getPivotPos(left, right):
+    # should use a random number between left and right
+    # if the random module is not allowed, we can just return
+    # left or right here, since we can assume the original array
+    # is not sorted
+    return random.randrange(left, right + 1)
+
+
+def partition(array, left, right):
+    pivotIndex = getPivotPos(left, right)
+    pivot = array[pivotIndex]
+    # swap the pivot element to the right most position first
+    swap(array, pivotIndex, right)
+
+    # use two pointers to traverse the array
+    leftBound = left
+    rightBount = right - 1
+    while leftBound <= rightBount:
+        if array[leftBound] < pivot:
+            leftBound += 1
+        elif array[rightBount] >= pivot:
+            rightBount -= 1
+        else:
+            # array[leftBound] >= pivot and array[rightBound] < pivot
+            swap(array, leftBound, rightBount)
+            leftBound += 1
+            rightBount -= 1
+    # here leftBound > rightBound
+    # . . . . rB lB . . . . . r
+    # all the numbers that are to the left of lB are < pivot
+    # move the pivot element back from the right most location
+    # to the location it should be;
+    swap(array, leftBound, right)
+    return leftBound
+
+
+def quickSort(array, left, right):
+    if left >= right:
+        return  # the only actual base case if when left == right
+
+    pivotPos = partition(array, left, right)
+    quickSort(array, left, pivotPos - 1)
+    quickSort(array, pivotPos + 1, right)
+
+
+def quick_sort(array):
+    if not array:
+        return array
+    quickSort(array, 0, len(array) - 1)
+    return array
+
+
+def rearrange_digits(input_list):
+    """
+    Rearrange Array Elements so as to form two number such that their sum is maximum.
+
+    Args:
+       input_list(list): Input List
+    Returns:
+       (int),(int): Two maximum sums
+    """
+    # sort the input list
+    # python sorting not allowed
+    # build two numbers
+    quick_sort(input_list)
+    n = len(input_list) // 2
+    str1 = ""
+    str2 = ""
+    for i in range(n):
+        str1 += str(input_list[2*i])
+        str2 += str(input_list[2*i + 1])
+    if len(input_list) % 2 != 0:
+        str1 += str(input_list[2*i + 2])
+
+    return [int(str1[-1::-1]), int(str2[-1::-1])]
+
+
+def test_function(test_case):
+    output = rearrange_digits(test_case[0])
+    solution = test_case[1]
+    if sum(output) == sum(solution):
+        print("Pass")
+    else:
+        print("Fail")
+
+
+test_function([[1, 2, 3, 4, 5], [542, 31]])
+test_function([[4, 6, 2, 5, 9, 8], [964, 852]])
